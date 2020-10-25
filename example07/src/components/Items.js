5adeb419cb66e5141d96add223a293bd1b6b623b
junilHwang <tjsdlf4261@naver.com>
2020-10-25 22:18:40 +0900

    feat: example07에 필터 및 입력 기능 추가

--- example07/src/components/Items.js
+++ example07/src/components/Items.js
@@ -1,8 +1,16 @@
 import Component from "../core/Component.js";
 
 export default class Items extends Component {
+  get filteredItems () {
+    const { isFilter, items } = this.$state;
+    return items.filter(({ active }) => (isFilter === 1 && active) ||
+                                        (isFilter === 2 && !active) ||
+                                        isFilter === 0);
+  }
+
   setup() {
     this.$state = {
+      isFilter: 0,
       items: [
         {
           seq: 1,
@@ -19,28 +27,37 @@ export default class Items extends Component {
   }
 
   template() {
-    const {items} = this.$state;
     return `
-      <ul>
-        ${items.map(({contents, active, seq}) => `
-          <li data-seq="${seq}">
-            ${contents}
-            <button class="toggleBtn" style="color: ${active ? '#09F' : '#F09'}">
-              ${active ? '활성' : '비활성'}
-            </button>
-            <button class="deleteBtn">삭제</button>
-          </li>
-        `).join('')}
-      </ul>
-      <button class="addBtn">추가</button>
+      <header>
+        <input type="text" class="appender" placeholder="아이템 내용 입력" />
+      </header>
+      <main>
+        <ul>
+          ${this.filteredItems.map(({contents, active, seq}) => `
+            <li data-seq="${seq}">
+              ${contents}
+              <button class="toggleBtn" style="color: ${active ? '#09F' : '#F09'}">
+                ${active ? '활성' : '비활성'}
+              </button>
+              <button class="deleteBtn">삭제</button>
+            </li>
+          `).join('')}
+        </ul>
+      </main>
+      <footer>
+        <button class="filterBtn" data-is-filter="0">전체 보기</button>
+        <button class="filterBtn" data-is-filter="1">활성 보기</button>
+        <button class="filterBtn" data-is-filter="2">비활성 보기</button>
+      </footer>
     `
   }
 
   setEvent() {
-    this.addEvent('click', '.addBtn', ({target}) => {
+    this.addEvent('keyup', '.appender', ({ key, target }) => {
+      if (key !== 'Enter') return;
       const {items} = this.$state;
       const seq = Math.max(0, ...items.map(v => v.seq)) + 1;
-      const contents = `item${seq}`;
+      const contents = target.value;
       const active = false;
       this.setState({
         items: [
@@ -63,6 +80,10 @@ export default class Items extends Component {
       const index = items.findIndex(v => v.seq === seq);
       items[index].active = !items[index].active;
       this.setState({items});
+    });
+
+    this.addEvent('click', '.filterBtn', ({ target }) => {
+      this.setState({ isFilter: Number(target.dataset.isFilter) });
     });
   }
 }
