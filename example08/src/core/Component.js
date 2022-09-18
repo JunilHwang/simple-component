a186df71d1ff5ddb0932b3dbe40e0ba24b1c93a8
JunilHwang <junil.h@kakao.com>
2022-09-18 14:27:49 +0900

    refactor: 이벤트 버블링 코드에서 불필요한 부분 제거

--- example08/src/core/Component.js
+++ example08/src/core/Component.js
@@ -22,10 +22,8 @@ export default class Component {
     this.render();
   }
   addEvent (eventType, selector, callback) {
-    const children = [ ...this.$target.querySelectorAll(selector) ];
-    const isTarget = (target) => children.includes(target) || target.closest(selector);
     this.$target.addEventListener(eventType, event => {
-      if (!isTarget(event.target)) return false;
+      if (!event.target.closest(selector)) return false;
       callback(event);
     })
   }
